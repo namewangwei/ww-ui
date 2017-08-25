6a835bdf11a8c09f118bf8c864a8314249a775c4
ww_7522 <ww_7522@163.com>
2017-08-25 13:54:26 +0800

    1

---
+++ src/components/message/message.vue
@@ -0,0 +1,18 @@
+<template>
+  <transition name="fade" id="message">
+      <div class="ww-message" v-if="1">12312312313hahahah</div>
+  </transition>
+</template>
+<script>
+  export default {
+    name: 'message'
+  }
+</script>
+<style>
+  #message > .ww-message {
+    font-size: 14px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    border: 1px solid #999;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// import MyPlugin from './index'
+
+Vue.config.productionTip = false
+
+import Message from './components/message/message'
+
+const MyPlugin = {}
+const version = 0.1.0
+MyPlugin.install = function (Vue, option) {
+  if (MyPlugin.install.installed) return
+
+  Vue.component(Message.name, Message)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  MyPlugin.install(window.Vue)
+}
+
+export default MyPlugin
+
+Vue.use(MyPlugin)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
